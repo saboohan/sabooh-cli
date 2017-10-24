7d3b2f61a40a0e2ec3d74430b9f7f56d9af378c7
Mostafa Asadi <mostafaasadi73@gmail.com>
2017-10-24 19:08:20 -0400

    uninstallation

--- sabooh_installer.sh
+++ sabooh_installer.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/mostafaasadi/sabooh
 cd sabooh
-sudo cp sabooh /usr/bin/
+sudo cp -v sabooh /usr/bin/
 sudo chmod +x /usr/bin/sabooh
-sudo cp sabooh.sh /etc/profile.d/
+sudo cp -v sabooh.sh /etc/profile.d/
---
+++ sabooh_uninstaller.sh
@@ -0,0 +1,2 @@
+sudo rm -fv /usr/bin/sabooh
+sudo rm -fv /etc/profile.d/sabooh.sh
