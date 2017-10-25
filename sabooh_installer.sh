12b41eeae21ccfc6e519c3482bab69ca62182b22
Mostafa Asadi <mostafaasadi73@gmail.com>
2017-10-25 04:14:25 -0400

    zsh

--- sabooh_installer.sh
+++ sabooh_installer.sh
@@ -2,4 +2,10 @@ git clone https://github.com/mostafaasadi/sabooh
 cd sabooh
 sudo cp -v sabooh /usr/bin/
 sudo chmod +x /usr/bin/sabooh
-sudo cp -v sabooh.sh /etc/profile.d/
+if [[ $SHELL == '/bin/bash' ]]; then
+	echo 'your shell : bash'
+	sudo cp -v sabooh.sh /etc/profile.d/
+elif [[ $SHELL == '/usr/bin/zsh' ]]; then
+	echo 'your zsh : zsh'
+	sudo echo 'sabooh' >> /etc/zsh/zprofile
+fi
