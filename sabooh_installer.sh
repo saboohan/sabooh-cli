5b70b54f7f4bb4023c3f81d4770cb08af7936dc7
Mostafa Asadi <mostafaasadi73@gmail.com>
2017-10-25 09:38:42 +0000

    Update sabooh_installer.sh

--- sabooh_installer.sh
+++ sabooh_installer.sh
@@ -4,7 +4,7 @@ sudo cp -v sabooh /usr/bin/
 sudo chmod +x /usr/bin/sabooh
 if [[ $SHELL == '/bin/bash' ]]; then
 	echo 'your shell : bash'
-	sudo echo 'sabooh' > /etc/profile.d/sabooh.sh
+	sudo sh -c echo 'sabooh' > /etc/profile.d/sabooh.sh
 elif [[ $SHELL == '/usr/bin/zsh' ]]; then
 	echo 'your zsh : zsh'
 	sudo echo 'sabooh' >> /etc/zsh/zprofile
