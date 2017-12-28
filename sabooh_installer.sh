0777a4c9afe8ef6f2b01f9911bc6115f156f8ed1
Mostafa Asadi <mostafaasadi73@gmail.com>
2017-12-28 13:31:18 +0330

    zsh startup fix

--- sabooh_installer.sh
+++ sabooh_installer.sh
@@ -5,7 +5,7 @@ sudo chmod +x /usr/bin/sabooh
 if [[ $SHELL == '/bin/bash' ]]; then
 	echo 'your shell : bash'
 	echo 'sabooh' | sudo tee /etc/profile.d/sabooh.sh
-elif [[ $SHELL == '/usr/bin/zsh' ]]; then
+elif [[ $SHELL == '/usr/bin/zsh' ]] || [[ $SHELL == '/bin/zsh' ]]; then
 	echo 'your zsh : zsh'
 	echo 'sabooh' | sudo tee -a /etc/zsh/zprofile
 fi
